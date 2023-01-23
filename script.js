7bcb929054cc819f6672578592ee390d28a5e195
“Your <fernandomartins.contato@outlook.com>
2023-01-23 14:00:32 -0300

    Função request login

--- script.js
+++ script.js
@@ -2,11 +2,7 @@ let userLogin;
 
 function login() {
     userLogin = prompt("Qual seu nome?");
-    if (userLogin !== "") {
-        postLogin();
-    } //else if () {
-
-    //}
+    postLogin();
 }
 login()
 
@@ -22,26 +18,28 @@ function postLogin() {
 // Promise do post login
 function loginSuccess(response) {
     console.log("Login Success");
+    setInterval(connectionRequest, 5000);
 }
 
 function loginFailure(erro) {
-    console.log("Login Failure");
-    console.log(erro);
+    console.log("Login falhou");
+    const statusError = erro.response.status;
+    console.log(statusError);
+    /*if (userLogin === "") {
+
+    }*/
+
 }
 
 // Request de conexao no servidor
 function connectionRequest() {
-    //setInterval(axios.post("https://mock-api.driven.com.br/api/v6/uol/status", { name: userLogin }), 5000);
-    //promise.then(requestSuccess);
-    //promise.catch(requestFailure);
+    const nameObject = { name: userLogin };
+    const promise = axios.post("https://mock-api.driven.com.br/api/v6/uol/status", nameObject);
+    promise.catch(disconnected);
 }
 
 
 // Promise da request
-function requestSuccess(response) {
-    console.log("Conexao OK");
-}
-function requestFailure(erro) {
-    console.log("Conexao caiu");
-    console.log(erro);
+function disconnected(req) {
+    console.log("Voce foi desconectado");
 }
